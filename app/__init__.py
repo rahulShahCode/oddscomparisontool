c97bacc712485df6fe10d93a1ecc66efb64fce5a
Rahul <rahul@shahus.net>
2024-07-13 14:59:18 -0400

    More deubgging

--- app/__init__.py
+++ app/__init__.py
@@ -4,42 +4,42 @@ from scripts.get_data import fetch_odds,process_games  # Import your function he
 import json 
 
 app = Flask(__name__)
-def retrieve_data(): 
-    sports = ['baseball_mlb']
-    processed_sports = {}
-    all_data = {} 
-    for sport in sports:
-        curr_data = fetch_odds(sport)
-        data = curr_data 
-        processed_games = process_games(curr_data)
-        processed_sports[sport] = processed_games
-    with open('data/processed_games.json', 'w') as f:
-        json.dump(processed_sports,f)
-    with open('data/curr_data.json', 'w') as f : 
-        json.dump(all_data, f)
+# def retrieve_data(): 
+#     sports = ['baseball_mlb']
+#     processed_sports = {}
+#     all_data = {} 
+#     for sport in sports:
+#         curr_data = fetch_odds(sport)
+#         data = curr_data 
+#         processed_games = process_games(curr_data)
+#         processed_sports[sport] = processed_games
+#     with open('data/processed_games.json', 'w') as f:
+#         json.dump(processed_sports,f)
+#     with open('data/curr_data.json', 'w') as f : 
+#         json.dump(all_data, f)
 
 @app.route('/write_dict')
 def write_dict(): 
     return jsonify(fetch_odds('baseball_mlb')), 200 
-@app.route('/')
-def home():
-    sport_key = request.args.get('sport', 'baseball_mlb')
-    with open('data/processed_games.json', 'r') as f: 
-        data = json.load(f)
-    if sport_key in data: 
-        print("Loading from file")
-        processed_games = data[sport_key]
-    else: 
-        games = fetch_odds(sport_key)
-        processed_games = process_games(games)
-        print("Fetched odds")
-    return render_template('odds.html', games=processed_games, sport=sport_key)
+# @app.route('/')
+# def home():
+#     sport_key = request.args.get('sport', 'baseball_mlb')
+#     with open('data/processed_games.json', 'r') as f: 
+#         data = json.load(f)
+#     if sport_key in data: 
+#         print("Loading from file")
+#         processed_games = data[sport_key]
+#     else: 
+#         games = fetch_odds(sport_key)
+#         processed_games = process_games(games)
+#         print("Fetched odds")
+#     return render_template('odds.html', games=processed_games, sport=sport_key)
 
 
-#Initial run
-retrieve_data()
+# #Initial run
+# retrieve_data()
 
-# Setup scheduler
-scheduler = BackgroundScheduler()
-scheduler.add_job(func=retrieve_data, trigger="interval", minutes=30)
-scheduler.start()
+# # Setup scheduler
+# scheduler = BackgroundScheduler()
+# scheduler.add_job(func=retrieve_data, trigger="interval", minutes=30)
+# scheduler.start()
